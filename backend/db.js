d256e34256db0f13d69d98d22dfb70512f6701b7
LEONEL ESTRADA <estradabarreraleonelarnulfo@gmail.com>
2026-09-09 18:42:36 -0600

    Delete MySQL connection pool setup
    Removed MySQL connection pool configuration.

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,1 @@
-const mysql = require("mysql2");
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",         // cambia por tu usuario de MariaDB
-    password: "leo",         // cambia por tu contraseña
-    database: "jami",
-    port: "3307",
-    waitForConnections: true,
-    connectionLimit: 10,
-});
-module.exports = pool.promise();
+
